bb513be34040701f9c2bff686c9153442b8f4bcd
Hoogachaka <thatdylan@gmail.com>
2023-03-01 21:10:54 -0600

    completed assignment

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Button, Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Paper, Container, Menu} from "@mui/material";
+import BasicMenu from './BasicMenu';
 
 const Import = (props) => {
-    // fill out this component
-
     return (
-        <p>Import Component</p>
-    )
-}
-
-export default Import
+      <Container maxWidth='lg'>
+        <Button variant='contained' color='primary' onClick={props.fetchMakes}>
+          IMPORT
+        </Button>
+        <h2> COUNT: {props.makes.length} </h2>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>MAKE</TableCell>
+              <TableCell>ACTIONS</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.makes.map((make, index) => (
+              <TableRow key={make.MakeId}>
+                <TableCell>{make.MakeId}</TableCell>
+                <TableCell>{make.MakeName}</TableCell>
+                <TableCell>
+                  <BasicMenu key={index} deleteMake={props.deleteMake} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Container>
+    );
+  };
+  
+  export default Import;
+  
